ceda130f600da7e225555de3a58d3bcb9df1cc81
genkio <5327840+genkio@users.noreply.github.com>
2026-07-27 19:40:12 +0900

    scrcpy-gui 2.2.3

--- Formula/scrcpy-gui.rb
+++ Formula/scrcpy-gui.rb
@@ -1,7 +1,7 @@
 class ScrcpyGui < Formula
   desc "Mirror and control Android devices from a GUI on top of scrcpy (genkio fork)"
   homepage "https://github.com/genkio/scrcpy-gui"
-  version "2.2.2"
+  version "2.2.3"
   license "Apache-2.0"
 
   depends_on :macos
@@ -9,12 +9,12 @@ class ScrcpyGui < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/genkio/scrcpy-gui/releases/download/v2.2.2/ScrcpyGui-2.2.2-arm64.zip"
-      sha256 "5701c0501f9831559784a1e0378638f4aa0806a52005f8bf5c0f73c38dc5e606"
+      url "https://github.com/genkio/scrcpy-gui/releases/download/v2.2.3/ScrcpyGui-2.2.3-arm64.zip"
+      sha256 "40a6d90d774731897b4050523bb2377cd53bf7e43008204d80c5f682cdd7456d"
     end
     on_intel do
-      url "https://github.com/genkio/scrcpy-gui/releases/download/v2.2.2/ScrcpyGui-2.2.2-x64.zip"
-      sha256 "41d8c54893fa1f553de0dec8208709abb8cbfc4961b09c5c316106df75062d28"
+      url "https://github.com/genkio/scrcpy-gui/releases/download/v2.2.3/ScrcpyGui-2.2.3-x64.zip"
+      sha256 "4fe579d2fb2ff4d29ac6309395ab4060b54d596e021c2763a61a88beca338f2e"
     end
   end
 
