7a92ca501c64e2ff00463c999a9b0ec1b67ab80c
genkio <5327840+genkio@users.noreply.github.com>
2026-08-12 10:43:09 +0900

    tui 0.13.0

--- Formula/tui.rb
+++ Formula/tui.rb
@@ -1,7 +1,7 @@
 class Tui < Formula
   desc "One launcher for cookie-stealth terminal readers: x, inoreader, slack, folo, reddit, douban"
   homepage "https://github.com/genkio/tui"
-  version "0.12.0"
+  version "0.13.0"
   license "MIT"
 
   # `o` renders a story in carbonyl.
@@ -9,23 +9,23 @@ class Tui < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/genkio/tui/releases/download/v0.12.0/tui_0.12.0_darwin_arm64.tar.gz"
-      sha256 "1f66d5b9614adb6fcb8937b875f5a0f90b6b75356d6bdf20e2cc330fac91cc37"
+      url "https://github.com/genkio/tui/releases/download/v0.13.0/tui_0.13.0_darwin_arm64.tar.gz"
+      sha256 "5ae62c9f447e11a3743f465b3acd2a78e7c233b852473c305ce6c1e5c283914e"
     end
     on_intel do
-      url "https://github.com/genkio/tui/releases/download/v0.12.0/tui_0.12.0_darwin_amd64.tar.gz"
-      sha256 "bd8386dc00158e9b236fd966de3741eb223f60adc7eccde8b68b664d83ed0ded"
+      url "https://github.com/genkio/tui/releases/download/v0.13.0/tui_0.13.0_darwin_amd64.tar.gz"
+      sha256 "9c75501743ca51e7fe1822b0a814aee2b8d8009cdd5a87e603ebac1ae2731679"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/genkio/tui/releases/download/v0.12.0/tui_0.12.0_linux_arm64.tar.gz"
-      sha256 "84c1efd039647147bb9af50554df71f2a1dd9556c531cbb5c4ab8e5ab5c60bcb"
+      url "https://github.com/genkio/tui/releases/download/v0.13.0/tui_0.13.0_linux_arm64.tar.gz"
+      sha256 "3ebf9d4fc70923e09a2623dcfa7713acb313409fc006dd837c7511a4b672eee0"
     end
     on_intel do
-      url "https://github.com/genkio/tui/releases/download/v0.12.0/tui_0.12.0_linux_amd64.tar.gz"
-      sha256 "86bef4d0f5df4e60c35cca0a46f0827220e9cc4ff49c8dfc34467744f277091c"
+      url "https://github.com/genkio/tui/releases/download/v0.13.0/tui_0.13.0_linux_amd64.tar.gz"
+      sha256 "ded345910ff17fae0855105089ecd886d40df681a649bec08599fa833fd1e659"
     end
   end
 
